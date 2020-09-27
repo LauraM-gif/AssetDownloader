0af24dfac839fd919e99ef18bca9ed84d67afc16
Ilias Pavidakis <ipavlidakis@gmail.com>
2020-09-27 13:52:37 +0100

    Change set to dictionary

--- Sources/AssetDownloader/Components/TaskQueue.swift
+++ Sources/AssetDownloader/Components/TaskQueue.swift
@@ -34,14 +34,14 @@ extension TaskQueue {
         _ taskProvider: @escaping (RestoredTask<AVURLAsset, AVAggregateAssetDownloadTask>) -> AVAssetDownloadDelegate?,
         cancelNonRestorableTasks: Bool = true
     ) {
-        var setOfTasks = Set<RestoredTask<AVURLAsset, AVAggregateAssetDownloadTask>>()
+        var setOfTasks = Dictionary<String ,RestoredTask<AVURLAsset, AVAggregateAssetDownloadTask>>()
         session.getAllTasks { [proxySessionDelegate] tasks in
             tasks
                 .compactMap { ($0 as? AVAggregateAssetDownloadTask)?.taskDescription != nil ? $0 as? AVAggregateAssetDownloadTask : nil  }
                 .map { RestoredTask<AVURLAsset, AVAggregateAssetDownloadTask>(name: $0.taskDescription!, url: $0.urlAsset, sessionTask: $0) }
-                .forEach { if !setOfTasks.contains($0) { setOfTasks.insert($0) } }
+                .forEach { if setOfTasks[$0.name] == nil { setOfTasks[$0.name] = $0 } }
 
-            for task in setOfTasks {
+            for task in setOfTasks.values {
                 guard let delegate = taskProvider(task) else {
                     if cancelNonRestorableTasks {
                         task.sessionTask.cancel()
